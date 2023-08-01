4ea8bff07184eedcb13e2ad2510a6010d7d7a4ff
samarthsoni3002 <88267506+samarthsoni3002@users.noreply.github.com>
2023-08-01 11:26:31 +0530

    Update Q2.c
    -Changed the functionality of input. Now, the program asks input from the user.
    - Solved the infinite printing issue.

--- Lab1/Q2.c
+++ Lab1/Q2.c
@@ -1,16 +1,35 @@
 #include <stdio.h>
 
 int main(void) {
-  int arr[5] = {1, 2, 3, 4, 5};
+  int arr[100];
+
+  int n;
+  printf("Enter how many numbers do you want to add:- ");
+  scanf("%d",&n);
+
+
+  printf("Enter numbers for the array:- ");
+  for(int i=0;i<n;i++){
+    scanf("%d",&arr[i]);
+  }
+
+  int a = 0;
+
   int key;
   printf("\n Enter the element to be Searched");
   scanf("%d", &key);
-  for (int i = 0; i < 5; i++) {
+
+
+    for (int i = 0; i < n; i++) {
     if (key == arr[i]) {
       printf("\n Element found:)");
-      break;
-    } else
-      printf("\n Element not found:(");
+      a=1;
+    }
   }
+
+    if(a==0){
+        printf("Element not found");
+    }
+
   return 0;
 }
