10b2e17127b3dc51699fa8bfac19e3989856925d
Vaibhavbansal15 <106023582+Vaibhavbansal15@users.noreply.github.com>
2023-08-01 11:14:34 +0530

    Update Q2.c
    Linear Search in C

--- Lab1/Q2.c
+++ Lab1/Q2.c
@@ -1,5 +1,16 @@
-#include<stdio.h>
-using namespace std;
-int main(){
-  cout<<" This is A simple C programme.";
+#include <stdio.h>
+
+int main(void) {
+  int arr[5] = {1, 2, 3, 4, 5};
+  int key;
+  printf("\n Enter the element to be Searched");
+  scanf("%d", &key);
+  for (int i = 0; i < 5; i++) {
+    if (key == arr[i]) {
+      printf("\n Element found:)");
+      break;
+    } else
+      printf("\n Element not found:(");
+  }
+  return 0;
 }
